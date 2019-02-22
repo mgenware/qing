592d6564d8a3138103268099f21ea05bee762757
Mgen <m@mgenware.com>
2019-02-22 20:14:45 +0800

    Add common styles

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import ls from './ls';
 
+// Styles
+import './app/styles/bulma.css';
+import './app/styles/app.css';
+
 Vue.config.productionTip = false;
 Vue.prototype.$ls = ls;
