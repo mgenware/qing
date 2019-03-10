9b42495e43f55f3d0a868cbbb7bf3bb301520f52
Mgen <m@mgenware.com>
2019-03-11 01:39:52 +0800

    Fix loader not reporting failed state

--- web/src/lib/loader.ts
+++ web/src/lib/loader.ts
@@ -1,7 +1,7 @@
 import Status from './status';
 import Result from './result';
 import ls from '../ls';
-import ErrorWithCode from './errorWithCode';
+import ErrorWithCode, { GENERIC_CODE } from './errorWithCode';
 
 export default class Loader {
   status: Status;
@@ -23,50 +23,54 @@ export default class Loader {
     this.status = new Status();
   }
 
-  async startAsync(settledCb?: () => void): Promise<object> {
-    if (this.status.isStarted) {
-      throw new Error('Loader should not be reused');
-    }
-    this.status.start();
-
-    let body = '';
-    const params = this.requestParams();
-    if (params) {
-      body = JSON.stringify(params);
-    }
-    const response = await fetch(this.requestURL(), {
-      method: 'POST',
-      body,
-      headers: {
-        'content-type': 'application/json',
-        credentials: 'same-origin',
-      },
-    });
-
-    // unlike a finally statement, settled callback is called before the catch block
-    // Suppose we need to something regardless of the result, e.g. hide the loading spinner, the problem is,
-    // finally is called after catch, but you need to do this immediately when it completes,
-    // this is where settled callback kicks in.
-    if (settledCb) {
-      settledCb();
-    }
-
-    // Handle HTTP status errors
-    if (!response.ok) {
-      const message =
-        response.status === 404 ? ls.error404 : response.statusText;
-
-      throw new ErrorWithCode(message);
-    } else {
-      const result = (await response.json()) as Result;
-      this._result = result;
-
-      // Check server return error
-      if (result.code) {
-        const message = result.message || `Error code ${result.code}`;
-        throw new ErrorWithCode(message, result.code);
+  async startAsync(): Promise<object> {
+    try {
+      if (this.status.isStarted) {
+        throw new Error('Loader should not be reused');
       }
-      return this.handleSuccess(result);
+      this.status.start();
+
+      let body = '';
+      const params = this.requestParams();
+      if (params) {
+        body = JSON.stringify(params);
+      }
+      const response = await fetch(this.requestURL(), {
+        method: 'POST',
+        body,
+        headers: {
+          'content-type': 'application/json',
+          credentials: 'same-origin',
+        },
+      });
+
+      // Handle HTTP status errors
+      if (!response.ok) {
+        const message =
+          response.status === 404 ? ls.error404 : response.statusText;
+
+        throw new ErrorWithCode(message);
+      } else {
+        const result = (await response.json()) as Result;
+        this._result = result;
+
+        // Check server return error
+        if (result.code) {
+          const message = result.message || `Error code ${result.code}`;
+          throw new ErrorWithCode(message, result.code);
+        }
+        return this.handleSuccess(result);
+      }
+    } catch (err) {
+      let errWithCode: ErrorWithCode;
+      if (err instanceof ErrorWithCode) {
+        errWithCode = err;
+      } else {
+        errWithCode = new ErrorWithCode(err.message, GENERIC_CODE);
+      }
+      this.status.failed(errWithCode);
+      // Rethrow the original error
+      throw err;
     }
   }
 
