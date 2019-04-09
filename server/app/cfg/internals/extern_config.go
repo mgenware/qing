ee16c6e002d100365508116f08bc6aeaa52d2493
Mgen <m@mgenware.com>
2019-04-09 17:27:54 +0800

    Remove nodejs extern libraries

--- server/app/cfg/internals/extern_config.go
+++ server/app/cfg/internals/extern_config.go
@@ -4,8 +4,6 @@ package internals
 type ExternConfig struct {
 	// Redis ...
 	Redis *ExternRedisConfig `json:"redis" validate:"required"`
-	// Node libs...
-	NodeLibs *ExternNodeLibConfig `json:"node_libs" validate:"required"`
 	// ConvertCmd ...
 	ConvertCmd string `json:"convert_cmd" validate:"required"`
 }
@@ -14,10 +12,4 @@ type ExternConfig struct {
 type ExternRedisConfig struct {
 	// Port is the port redis will be connect to.
 	Port int `json:"port" validate:"gt=0"`
-}
-
-// ExternNodeLibConfig ...
-type ExternNodeLibConfig struct {
-	// Dir is the root directory of node libs.
-	Dir string `json:"dir" validate:"required"`
 }
--- server/app/extern/extern.go
+++ server/app/extern/extern.go
@@ -3,13 +3,11 @@ package extern
 import (
 	"fmt"
 	"qing/app/cfg"
-	"qing/app/extern/nl"
 	"qing/app/extern/redisx"
 )
 
 type Extern struct {
 	RedisConn *redisx.Conn
-	NL        *nl.NL
 }
 
 func MustSetupExtern(config *cfg.Config) *Extern {
@@ -21,13 +19,6 @@ func MustSetupExtern(config *cfg.Config) *Extern {
 		panic(fmt.Errorf("Ping to redis failed, %v", err.Error()))
 	}
 	result.RedisConn = redisConn
-
-	// node libs
-	nlObj, err := nl.NewNL(config.Extern.NodeLibs.Dir)
-	if err != nil {
-		panic(fmt.Errorf("Ping to node libs failed, %v", err.Error()))
-	}
-	result.NL = nlObj
 
 	return result
 }
