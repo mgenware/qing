fd983d6f0835a1cb83711df1a6e3e333f066ed3b
Mgen <m@mgenware.com>
2020-02-12 23:47:59 +0800

    Scroll to user posts when page param is present

--- web/src/profile/profileEntry.ts
+++ web/src/profile/profileEntry.ts
@@ -6,3 +6,12 @@ if (!wind.isPrevEnabled) {
 if (!wind.isNextEnabled) {
   document.getElementById('m-next-btn')?.classList.add('content-disabled');
 }
+
+// Scroll to user posts only if `page` query string is present.
+const qs = new URLSearchParams(window.location.search);
+if (qs.get('page')) {
+  setTimeout(
+    () => document.getElementById('m-profile-posts')?.scrollIntoView(true),
+    800,
+  );
+}
