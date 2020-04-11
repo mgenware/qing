b68859a259472963998b7d9784b7317b4457104f
Mgen <m@mgenware.com>
2020-04-11 23:33:57 +0800

    Add input-view component

---
+++ web/src/ui/form/input-error-view.ts
@@ -0,0 +1,42 @@
+import { html, customElement, css } from 'lit-element';
+import * as lp from 'lit-props';
+import BaseElement from 'baseElement';
+
+@customElement('input-error-view')
+export class InputErrorView extends BaseElement {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        div {
+          display: inline-block;
+          padding: 5px 8px;
+          background-color: var(--danger-back-color);
+          color: var(--danger-fore-color);
+          border-radius: 4px;
+          margin-bottom: 10px;
+        }
+      `,
+    ];
+  }
+
+  @lp.string message = '';
+
+  render() {
+    const { message } = this;
+    if (!message) {
+      return html``;
+    }
+    return html`
+      <div>
+        ${message}
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'input-error-view': InputErrorView;
+  }
+}
---
+++ web/src/ui/form/input-view.ts
@@ -0,0 +1,107 @@
+import { html, customElement, css } from 'lit-element';
+import * as lp from 'lit-props';
+import BaseElement from 'baseElement';
+import './input-error-view';
+
+const inputID = 'input-id';
+
+export type InputType =
+  | 'hidden'
+  | 'text'
+  | 'search'
+  | 'tel'
+  | 'url'
+  | 'email'
+  | 'password'
+  | 'datetime'
+  | 'date'
+  | 'month'
+  | 'week'
+  | 'time'
+  | 'datetime-local'
+  | 'number'
+  | 'range'
+  | 'color'
+  | 'checkbox'
+  | 'radio'
+  | 'file'
+  | 'submit'
+  | 'image'
+  | 'reset'
+  | 'button';
+
+@customElement('input-view')
+export class InputView extends BaseElement {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        input {
+          font-size: 1rem;
+          max-width: 100%;
+          width: 100%;
+          margin-bottom: 0.8rem;
+          line-height: 1.5;
+          padding: 0.25rem 0.4rem;
+          background-color: transparent;
+          color: var(--default-fore-color);
+          border: 1px solid var(--default-separator-color);
+          padding: 0.35rem 0.6rem;
+          border-radius: 5px;
+        }
+
+        input:invalid {
+          border-color: var(--danger-fore-color);
+        }
+      `,
+    ];
+  }
+
+  @lp.string label = '';
+  @lp.bool required = false;
+  @lp.string type: InputType = 'text';
+  @lp.string value = '';
+
+  @lp.string private validationMessage = '';
+  private inputElement!: HTMLInputElement;
+
+  firstUpdated() {
+    this.inputElement = this.mustGetShadowElement(inputID);
+  }
+
+  render() {
+    return html`
+      <div>
+        <label class="form-label" for=${inputID}>${this.label}</label>
+        <input
+          id=${inputID}
+          ?required=${this.required}
+          type=${this.type}
+          value=${this.value}
+          @input=${this.handleInput}
+        />
+        <input-error-view message=${this.validationMessage}></input-error-view>
+      </div>
+    `;
+  }
+
+  checkValidity(): boolean {
+    return this.inputElement.checkValidity();
+  }
+
+  private handleInput(e: Event) {
+    const input = e.target as HTMLInputElement;
+    this.validationMessage = input.validationMessage;
+    this.dispatchEvent(
+      new CustomEvent<string>('onChange', {
+        detail: input.value,
+      }),
+    );
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'input-view': InputView;
+  }
+}
