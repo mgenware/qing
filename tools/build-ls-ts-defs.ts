81eac45b3598c3e48620f89ad1d5a30f408739ab
Mgen <m@mgenware.com>
2020-04-25 22:37:43 +0800

    Convert build-ls-ts-defs.js to TS

--- tools/build-ls-ts-defs.ts
+++ tools/build-ls-ts-defs.ts
@@ -1,14 +1,14 @@
 // Build type definitions for `src/ls.ts`.
 
-const fs = require('fs').promises;
+import { promises as fsPromises } from 'fs';
 
 (async () => {
-  const lsString = await fs.readFile('./langs/en.json', 'utf8');
+  const lsString = await fsPromises.readFile('./langs/en.json', 'utf8');
   const lsObj = JSON.parse(lsString);
   let out = `export default interface LSDefs {\n`;
   for (const key of Object.keys(lsObj)) {
     out += `  ${key}: string;\n`;
   }
   out += '}\n';
-  await fs.writeFile('./src/lsDefs.ts', out);
+  await fsPromises.writeFile('./src/lsDefs.ts', out);
 })();
