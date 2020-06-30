fdaa6cce82611e9d74c1e99c13b08159434b5fcf
Mgen <m@mgenware.com>
2020-06-30 23:58:10 +0800

    Remove unused methods in HTML lib

--- web/src/lib/htmlLib.ts
+++ web/src/lib/htmlLib.ts
@@ -2,39 +2,6 @@ import { TemplateResult } from 'lit-element';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render, html } from 'lit-html';
 
-export function parseDOMString(str: string): Element | null {
-  if (!str) {
-    return null;
-  }
-  const tmp = document.implementation.createHTMLDocument();
-  tmp.body.innerHTML = str;
-  const list = tmp.body.children;
-  if (list && list.length) {
-    return list.item(0);
-  }
-  return null;
-}
-
-export function removeElement(element: Element) {
-  const { parentNode } = element;
-  if (parentNode) {
-    parentNode.removeChild(element);
-  }
-}
-
-export function wrap(el: Element, wrapper: Element | null) {
-  if (!el.parentNode || !wrapper) {
-    return;
-  }
-  el.parentNode.insertBefore(wrapper, el);
-  wrapper.appendChild(el);
-}
-
-export function constructURL(params: Record<string, string>): string {
-  // params as URLSearchParams: just to mute type error
-  return new URLSearchParams(params).toString();
-}
-
 export function ready(fn: () => void) {
   if (document.readyState !== 'loading') {
     fn();
