30943088a2336e503d0721c3c032b027b462e0f5
Mgen <m@mgenware.com>
2020-10-08 23:50:27 +0800

    Extract common string literals to a constant

--- web/src/profile/profileEntry.ts
+++ web/src/profile/profileEntry.ts
@@ -1,17 +1,16 @@
 import wind from './wind';
 
+const disabledCSS = 'content-disabled';
+
 if (!wind.isPrevEnabled) {
-  document.getElementById('m-prev-btn')?.classList.add('content-disabled');
+  document.getElementById('m-prev-btn')?.classList.add(disabledCSS);
 }
 if (!wind.isNextEnabled) {
-  document.getElementById('m-next-btn')?.classList.add('content-disabled');
+  document.getElementById('m-next-btn')?.classList.add(disabledCSS);
 }
 
 // Scroll to user posts only if `page` query string is present.
 const qs = new URLSearchParams(window.location.search);
 if (qs.get('page')) {
-  setTimeout(
-    () => document.getElementById('m-profile-posts')?.scrollIntoView(true),
-    800,
-  );
+  setTimeout(() => document.getElementById('m-profile-posts')?.scrollIntoView(true), 800);
 }
