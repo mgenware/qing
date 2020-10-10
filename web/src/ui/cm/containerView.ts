cfda3b1c87b12fe1c90af091f9dbb401903c83bb
Mgen <m@mgenware.com>
2020-10-10 21:06:31 +0800

    Use container-view everywhere

--- web/src/ui/cm/containerView.ts
+++ web/src/ui/cm/containerView.ts
@@ -1,5 +1,8 @@
 import { html, customElement, css } from 'lit-element';
 import BaseElement from 'baseElement';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { classMap } from 'lit-html/directives/class-map';
+import * as lp from 'lit-props';
 
 @customElement('container-view')
 export class ContainerView extends BaseElement {
@@ -10,13 +13,31 @@ export class ContainerView extends BaseElement {
         :host {
           display: block;
         }
+
+        .margin {
+          padding-top: 1rem;
+          padding-bottom: 1.2rem;
+          margin-bottom: 1.2rem;
+        }
       `,
     ];
   }
 
+  // If true, no extra spacing on top and bottom edges.
+  @lp.bool slim = false;
+
   render() {
     // The `container` is from bootstrap grid styles.
-    return html` <div class="container"><slot></slot></div> `;
+    return html`
+      <div
+        class=${classMap({
+          container: true,
+          margin: !this.slim,
+        })}
+      >
+        <slot></slot>
+      </div>
+    `;
   }
 }
 
