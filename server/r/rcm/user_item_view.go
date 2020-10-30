dd8fef35ec5649816ca7d60f6dda364c50d590f5
Mgen <m@mgenware.com>
2020-10-31 06:04:16 +0800

    Extract user item view into a single file

---
+++ server/r/rcm/user_item_view.go
@@ -0,0 +1,33 @@
+package rcm
+
+import (
+	"qing/app"
+	"qing/lib/validator"
+	"time"
+)
+
+// UserItemViewData contains properties required to generate a user item view.
+type UserItemViewData struct {
+	ItemEID        string
+	UserEID        string
+	UserName       string
+	UserURL        string
+	UserIconURL    string
+	ItemCreatedAt  time.Time
+	ItemModifiedAt *time.Time
+}
+
+var vUserView = app.TemplateManager.MustParseView("/cm/userItemView.html")
+
+// GetUserItemViewHTML returns user item view HTML with the given params.
+func GetUserItemViewHTML(uid uint64, name, iconName string, itemEID string, itemCreated time.Time, itemModified *time.Time) string {
+	d := &UserItemViewData{}
+	d.ItemEID = itemEID
+	d.UserEID = validator.EncodeID(uid)
+	d.UserName = name
+	d.UserURL = app.URL.UserProfile(uid)
+	d.UserIconURL = app.URL.UserIconURL50(uid, iconName)
+	d.ItemCreatedAt = itemCreated
+	d.ItemModifiedAt = itemModified
+	return vUserView.MustExecuteToString(d)
+}
