80984aceb83a9b0327476443d0848f463b5646d7
Mgen <m@mgenware.com>
2020-12-04 22:29:02 +0800

    Preprocess dev flags before building

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -4,8 +4,16 @@ import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import litcss from 'rollup-plugin-lit-css';
+import replace from '@rollup/plugin-replace';
 
 const isProd = process.env.NODE_ENV == 'production';
+
+function preprocessNodeEnv() {
+  return replace({
+    'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+    __qing_dev__: !isProd,
+  });
+}
 
 const plugins = [
   nodeResolve({
@@ -18,10 +26,11 @@ const plugins = [
   typescript({
     tsconfig: './tsconfig-build.json',
   }),
+  preprocessNodeEnv(),
 ];
 
 if (isProd) {
-  plugins.push(terser());
+  plugins.push(terser(), preprocessNodeEnv());
 }
 
 const input = [
