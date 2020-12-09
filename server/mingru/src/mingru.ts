6def45d819f2fd4129a2f0c128c3e97619e90bfd
Mgen <m@mgenware.com>
2020-12-09 18:39:03 +0800

    Use tsc instead of ts-node to compile mingru source files

--- server/mingru/src/mingru.ts
+++ server/mingru/src/mingru.ts
@@ -1,11 +1,12 @@
 import * as mr from 'mingru';
+import * as nodepath from 'path';
 import actions from './actions/actions';
 import models from './models/models';
 
 (async () => {
   const dialect = new mr.MySQL();
   // Build Go code to '../da/` directory
-  const builder = new mr.Builder(dialect, '../da/', {
+  const builder = new mr.Builder(dialect, nodepath.join(__dirname, '../../da/'), {
     cleanBuild: true,
     jsonEncoding: {
       encodingStyle: mr.JSONEncodingStyle.camelCase,
