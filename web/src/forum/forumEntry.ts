098f8f164f608337216a6ad9573c9495a8f5382a
Mgen <m@mgenware.com>
2021-01-19 21:44:16 +0800

    Hightlight tabs in forum entry

--- web/src/forum/forumEntry.ts
+++ web/src/forum/forumEntry.ts
@@ -1,2 +1,16 @@
+import { ready } from 'lib/htmlLib';
+import { keyTab } from 'sharedConstants';
 import 'ui/content/headingView';
 import 'ui/lists/tabView';
+import { tabViewActiveClass } from 'ui/lists/tabView';
+
+const defaultHighlightedTab = 'threads';
+
+ready(() => {
+  // Highlight the selected tab.
+  const qs = new URLSearchParams(window.location.search);
+  const tab = qs.get(keyTab);
+  document
+    .getElementById(`m-forum-tab-${tab ?? defaultHighlightedTab}`)
+    ?.classList.add(tabViewActiveClass);
+});
