6ab36c70ae1e5f26c97ecd04d99a93aaa15c30bc
Mgen <m@mgenware.com>
2021-01-23 23:54:20 +0800

    Fix deepEqual deprecated

--- web/ut/arrayUtils.test.ts
+++ web/ut/arrayUtils.test.ts
@@ -2,5 +2,5 @@ import * as assert from 'assert';
 import { skipIndex } from '../src/lib/arrayUtils';
 
 it('skipIndex', () => {
-  assert.deepEqual(skipIndex([1, 2, 3], 1), [1, 3]);
+  assert.deepStrictEqual(skipIndex([1, 2, 3], 1), [1, 3]);
 });
