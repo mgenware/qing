8e409f421a6d080b31c7998cc838dfc8808018e5
Mgen <m@mgenware.com>
2021-02-02 20:00:49 +0800

    ut: web: checks.ts

---
+++ web/src/checks.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'qing-t';
+import { CHECK } from './checks';
+
+it('Check ignored in prod mode', () => {
+  expect(() => CHECK(0)).to.not.throw();
+});
+
+it('Check fulfilled', () => {
+  expect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).__qing_dev__ = true;
+    CHECK(0);
+  }).to.throw('Assertion failed');
+});
