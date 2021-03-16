991474d906e80d6010d7d5f651a3a7d69033c46a
Mgen <m@mgenware.com>
2021-03-16 23:54:02 +0800

    Remove unused views

--- web/src/com/user/views/findUserItemView.test.ts
+++
@@ -1,8 +0,0 @@
-import { html, fixture, tDOM } from 'qing-t';
-import './findUserItemView';
-
-it('Display', async () => {
-  const el = await fixture<HTMLElement>(html`<find-user-item-view></find-user-item-view>`);
-
-  tDOM.isBlockElement(el);
-});
