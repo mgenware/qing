3928dd2a8eb58dcc6b22fe6f2bf250e3565dbc5e
Mgen <m@mgenware.com>
2021-03-18 23:31:59 +0800

    Add an event emitter

---
+++ web/src/lib/eventEmitter.ts
@@ -0,0 +1,29 @@
+export type EventEmitterAction = (arg: unknown) => void;
+
+export class EventEmitter {
+  private actions: Record<string, EventEmitterAction[]> = [];
+
+  addListener(name: string, cb: EventEmitterAction): EventEmitterAction {
+    (this.actions[name] ??= [])).push(cb);
+
+    return () => {
+      const list = this.actions[name];
+      if (!list) {
+        return;
+      }
+      const idx = list.indexOf(cb);
+      if (idx >= 0) {
+        list.splice(idx, 1);
+      }
+    };
+  }
+
+  emit(name: string, arg?: unknown): boolean {
+    const list = this.actions[name];
+    if (!list) {
+      return false;
+    }
+    list.forEach(cb => cb(arg));
+    return true;
+  }
+}
