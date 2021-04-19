859ce3daeae29bc30a977e9dbf89bb988aa6ab48
Mgen <m@mgenware.com>
2021-04-19 23:54:04 +0800

    API support for `da.Reply.SelectRepliesWithLike`

--- server/r/api/pub/cmt_api/get_cmts.go
+++ server/r/api/pub/cmt_api/get_cmts.go
@@ -72,37 +72,42 @@ func getCmts(w http.ResponseWriter, r *http.Request) handler.JSON {
 	page := validator.GetPageParamFromDict(params)
 
 	db := appDB.DB()
+	var respData GetCmtsRespData
+	var items []da.CmtData
+	var hasNext bool
+	var err error
+
 	// Selecting replies.
 	if parentCmtID != 0 {
-		replies, hasNext, err := da.Reply.SelectReplies(db, parentCmtID, page, kCmtPageSize)
+		if uid == 0 {
+			items, hasNext, err = da.Reply.SelectReplies(db, parentCmtID, page, kCmtPageSize)
+		} else {
+			items, hasNext, err = da.Reply.SelectRepliesWithLike(db, uid, parentCmtID, page, kCmtPageSize)
+		}
 		if err != nil {
 			app.PanicIfErr(err)
 		}
 
-		respData := newGetRepliesRespData(replies, hasNext)
+		respData := newGetRepliesRespData(items, hasNext)
 		return resp.MustComplete(respData)
 	}
 
 	// Selecting comments.
 	hostID := validator.MustGetIDFromDict(params, "hostID")
 	hostType := validator.MustGetIntFromDict(params, "hostType")
-	var respData GetCmtsRespData
-	var cmts []da.CmtData
-	var hasNext bool
-	var err error
 
 	switch hostType {
 	case defs.Shared.EntityPost:
 		{
 			if uid == 0 {
-				cmts, hasNext, err = da.Post.SelectCmts(db, hostID, page, kCmtPageSize)
+				items, hasNext, err = da.Post.SelectCmts(db, hostID, page, kCmtPageSize)
 			} else {
-				cmts, hasNext, err = da.Post.SelectCmtsWithLike(db, uid, hostID, page, kCmtPageSize)
+				items, hasNext, err = da.Post.SelectCmtsWithLike(db, uid, hostID, page, kCmtPageSize)
 			}
 			if err != nil {
 				app.PanicIfErr(err)
 			}
-			respData = newGetCmtsRespData(cmts, hasNext)
+			respData = newGetCmtsRespData(items, hasNext)
 		}
 	default:
 		{
