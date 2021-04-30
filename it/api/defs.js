3a44621dccf431b671a234135951b173b56ba3c0
Mgen <m@mgenware.com>
2021-04-30 23:17:20 +0800

    Delete tmp post in add post tests

--- it/api/defs.js
+++ it/api/defs.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2021 The Qing Project. All rights reserved.
+ *
+ * Use of this source code is governed by a license that can
+ * be found in the LICENSE file.
+ */
+
 export default {
   entityPost: 1,
   entityCmt: 2,
--- it/api/pri/compose/set_post_test.js
+++ it/api/pri/compose/set_post_test.js
@@ -5,11 +5,22 @@
  * be found in the LICENSE file.
  */
 
-import { post, usr, assUtil, ass } from '../../t.js';
+import { post, usr, assUtil, ass, sendPost } from '../../t.js';
 import defs from '../../defs.js';
 
 const url = 'pri/compose/set-post';
 const postContent = { contentHTML: '_POST_', title: '_TITLE_' };
+
+const deletePostURL = 'pri/compose/set-post';
+const postIDRegex = /\/p\/([a-z0-9]+)$/;
+
+/**
+ * @param {string} id
+ * @returns {Promise<APIResult>}
+ */
+async function deletePost(id) {
+  return sendPost({ url: deletePostURL, body: { id, entityType: defs.entityPost } });
+}
 
 post(
   'set-post: add-post',
@@ -18,7 +29,10 @@ post(
     body: { entityType: defs.entityPost, content: postContent },
   },
   usr.user,
-  (r) => {
-    ass.regex(r.d, /\/p\/[a-z0-9]+/);
+  async (r) => {
+    ass.regex(r.d, postIDRegex);
+    const id = postIDRegex.exec(r.d);
+    ass.t(id);
+    await deletePost(id);
   },
 );
