a22df3fcbdc7103635cfd3871f1d6b218106c76c
Mgen <m@mgenware.com>
2021-05-21 22:04:14 +0800

    Use throw-if-arg-empty to validate arguments in IT project

--- it/br/c/like_helper.js
+++ it/br/c/like_helper.js
@@ -5,9 +5,11 @@
  * be found in the LICENSE file.
  */
 
+import { throwIfEmpty } from 'throw-if-arg-empty';
 import { ass } from '../t.js';
 
 export async function checkLikes(likeAppEl, value, selected) {
+  throwIfEmpty(likeAppEl, 'likeAppEl');
   const numEl = await likeAppEl.$('pierce/like-view > qing-button > span.num');
   ass.t(numEl);
   ass.e(await numEl.evaluate((el) => el.textContent), `${value}`);
