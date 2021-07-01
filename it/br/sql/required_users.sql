be2c6f9fb5fa80ee733eab8f958561d29ff1cdeb
Mgen <m@mgenware.com>
2021-07-02 00:00:04 +0800

    Fix missing copyright

--- it/br/sql/required_users.sql
+++ it/br/sql/required_users.sql
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2021 The Qing Project. All rights reserved.
+ *
+ * Use of this source code is governed by a license that can
+ * be found in the LICENSE file.
+ */
+
 INSERT INTO `user` (`id`, `email`, `name`, `icon_name`, `url_name`, `created_at`, `company`, `website`, `location`, `bio`, `admin`, `status`)
 VALUES
 	(101, 'm@admin.com', 'ADMIN', 'admin.png', NULL, '1990-10-27 10:20:30', 'ADMIN_COMPANY', 'ADMIN_WEBSITE', '', NULL, 1, ''),
