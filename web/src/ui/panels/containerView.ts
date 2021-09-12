790201383dda47fbda76962d8e0d84035a16449b
Mgen <m@mgenware.com>
2021-09-13 00:23:57 +0800

    Remove the slim property from container-view

--- web/src/ui/panels/containerView.ts
+++ web/src/ui/panels/containerView.ts
@@ -5,8 +5,7 @@
  * be found in the LICENSE file.
  */
 
-import { html, customElement, css, BaseElement, lp } from 'll';
-import { classMap } from 'lit/directives/class-map.js';
+import { html, customElement, css, BaseElement } from 'll';
 
 @customElement('container-view')
 export class ContainerView extends BaseElement {
@@ -17,28 +16,13 @@ export class ContainerView extends BaseElement {
         :host {
           display: block;
         }
-
-        .with-margin {
-          padding-top: 1rem;
-          padding-bottom: 1.2rem;
-          margin-bottom: 1.2rem;
-        }
       `,
     ];
   }
 
-  // If true, no extra spacing on top and bottom edges.
-  @lp.bool slim = false;
-
   render() {
-    // The `container` is from bootstrap grid styles.
     return html`
-      <div
-        class=${classMap({
-          container: true,
-          'with-margin': !this.slim,
-        })}
-      >
+      <div class="container">
         <slot></slot>
       </div>
     `;
