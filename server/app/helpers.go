38e154148ebcfd6fa362941e65e116c6b61b7d6b
Mgen <m@mgenware.com>
2021-09-16 00:49:35 +0800

    Add PanicIfErrEx to use go 1.13 error wrapping

--- server/app/helpers.go
+++ server/app/helpers.go
@@ -1,8 +1,18 @@
 package app
 
+import "fmt"
+
 // PanicIfErr panics if the given err is not nil.
 func PanicIfErr(err error) {
 	if err != nil {
+		panic(err)
+	}
+}
+
+// PanicIfErrEx panics if the given err is not nil.
+func PanicIfErrEx(err error, info string) {
+	if err != nil {
+		err = fmt.Errorf("%v: %v", info, err)
 		panic(err)
 	}
 }
