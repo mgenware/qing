23596a5a910a2cfadaa910447d333924111151a9
Mgen <m@mgenware.com>
2022-01-16 02:37:07 +0800

    [WIP] [db]  Update cmt model

--- lib/dev/da/models/cmt/cmt.ts
+++ lib/dev/da/models/cmt/cmt.ts
@@ -8,26 +8,17 @@
 import * as mm from 'mingru-models';
 import user from '../user/user.js';
 
-export class CmtCore extends mm.Table {
-  // Common fields for both cmt and reply.
+export class Cmt extends mm.Table {
   id = mm.pk();
+  parent_id = mm.uBigInt().nullable;
   content = mm.text().setModelName('ContentHTML');
   user_id = user.id;
+  reply_count = mm.uInt().default(0);
+  likes = mm.uInt().default(0);
   created_at = mm.datetime('utc').setModelName('RawCreatedAt');
   modified_at = mm.datetime('utc').setModelName('RawModifiedAt');
 }
 
-export class Cmt extends CmtCore {
-  reply_count = mm.uInt().default(0);
-  likes = mm.uInt().default(0);
-}
-
 export const cmt = mm.table(Cmt);
 
-export class Reply extends CmtCore {
-  to_user_id = user.id;
-  parent_id = cmt.id;
-  likes = mm.uInt().default(0);
-}
-
-export const reply = mm.table(Reply);
+export default cmt;
