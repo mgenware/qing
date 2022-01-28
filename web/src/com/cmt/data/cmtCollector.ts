b6b52f235c188757244c6f2a73418e0610e556d6
Mgen <m@mgenware.com>
2022-01-28 23:44:04 +0800

    Wrap common ctors of CmtCollector in static funcs

--- web/src/com/cmt/data/cmtCollector.ts
+++ web/src/com/cmt/data/cmtCollector.ts
@@ -16,7 +16,7 @@ export type GetCmtsInputsWithoutPage = Exclude<GetCmtsInputs, 'page'>;
 export type GetRepliesInputsWithoutPage = Exclude<GetRepliesInputs, 'page'>;
 
 export default class CmtCollector extends ItemCollector<Cmt> {
-  constructor(
+  private constructor(
     initialTotalCount: number,
     public cmtInputs: GetCmtsInputsWithoutPage | undefined,
     public replyInputs: GetRepliesInputsWithoutPage | undefined,
@@ -24,6 +24,30 @@ export default class CmtCollector extends ItemCollector<Cmt> {
     public itemsChanged: (e: ItemsChangedEvent<Cmt>) => void,
   ) {
     super(initialTotalCount, (it) => it.id, loadingStatusChanged, itemsChanged);
+  }
+
+  static rootCmts(
+    cmtInputs: GetCmtsInputsWithoutPage,
+    loadingStatusChanged: (status: LoadingStatus) => void,
+    itemsChanged: (e: ItemsChangedEvent<Cmt>) => void,
+  ) {
+    // Total count is useless when collecting root cmts.
+    return new CmtCollector(0, cmtInputs, undefined, loadingStatusChanged, itemsChanged);
+  }
+
+  static replies(
+    initialTotalCount: number,
+    replyInputs: GetRepliesInputsWithoutPage,
+    loadingStatusChanged: (status: LoadingStatus) => void,
+    itemsChanged: (e: ItemsChangedEvent<Cmt>) => void,
+  ) {
+    return new CmtCollector(
+      initialTotalCount,
+      undefined,
+      replyInputs,
+      loadingStatusChanged,
+      itemsChanged,
+    );
   }
 
   protected createLoader(): Loader<ItemsLoadedResp<Cmt>> {
