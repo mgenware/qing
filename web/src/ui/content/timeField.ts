a2de58e53b605e21566a2fc39d857b74a08983d3
Mgen <m@mgenware.com>
2022-04-07 22:27:52 +0800

    Ditch date-fns

--- web/src/ui/content/timeField.ts
+++ web/src/ui/content/timeField.ts
@@ -8,8 +8,17 @@
 import { BaseElement, customElement, html } from 'll';
 import * as lp from 'lit-props';
 import ls from 'ls';
-import { formatRelative, Locale } from 'date-fns';
-import { enUS, zhCN } from 'date-fns/locale';
+
+var units = {
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: (24 * 60 * 60 * 1000 * 365) / 12,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000,
+  second: 1000,
+};
+
+var rtf = new Intl.RelativeTimeFormat(ls._lang, { numeric: 'auto' });
 
 @customElement('time-field')
 export class TimeField extends BaseElement {
@@ -36,7 +45,7 @@ export class TimeField extends BaseElement {
   private formatDate(s: string): string {
     try {
       const date = this.stringToDate(s);
-      return formatRelative(date, new Date(), { locale: this.locale });
+      return this.formatRelativeDate(date);
     } catch (err) {
       return '[Invalid date]';
     }
@@ -44,6 +53,19 @@ export class TimeField extends BaseElement {
 
   private stringToDate(str: string): Date {
     return new Date(str);
+  }
+
+  // https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time?answertab=modifieddesc#tab-top
+  private formatRelativeDate(date: Date) {
+    var elapsed = date.getTime() - new Date().getTime();
+
+    // "Math.abs" accounts for both "past" & "future" scenarios
+    for (var [k, v] of Object.entries(units)) {
+      if (Math.abs(elapsed) > v || k === 'second') {
+        return rtf.format(Math.round(elapsed / v), k as Intl.RelativeTimeFormatUnit);
+      }
+    }
+    return '';
   }
 }
 
