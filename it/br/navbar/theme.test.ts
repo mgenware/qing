9f8656ea54133da1f8c6ecd8965a434fe2dcd96b
Mgen <m@mgenware.com>
2022-07-06 02:59:35 +0800

    Add br tests for theme changes

--- it/br/navbar/theme.test.ts
+++ it/br/navbar/theme.test.ts
@@ -5,10 +5,11 @@
  * be found in the LICENSE file.
  */
 
-import { test, $, Element } from 'br';
+import { test, $, Element, Page } from 'br';
 import { navbarThemeMenuSel, clickNavbarThemeMenu } from 'br/com/navbar/navbar';
 
 const themeOptionText = ['Light theme', 'Dark theme', 'Device theme'];
+const cssDarkTheme = 'theme-dark';
 
 async function checkCheckbox(el: Element, checked: boolean, text: string) {
   await el.shouldContainTextContent(text);
@@ -28,10 +29,77 @@ async function checkThemeMenu(idx: number, menuEl: Element) {
   );
 }
 
-test('Navbar - default theme', async ({ page }) => {
+async function checkTheme(page: Page, dark: boolean) {
+  if (dark) {
+    return page.body.shouldHaveClass(cssDarkTheme);
+  }
+  return page.body.shouldNotHaveClass(cssDarkTheme);
+}
+
+test('Navbar - Default theme', async ({ page }) => {
   const p = $(page);
   await p.goto('/', null);
   await clickNavbarThemeMenu(p);
   const menuEl = p.$(navbarThemeMenuSel);
   await checkThemeMenu(0, menuEl);
+  await checkTheme(p, false);
+});
+
+test('Navbar - Change to dark theme', async ({ page }) => {
+  const p = $(page);
+  await p.goto('/', null);
+  await clickNavbarThemeMenu(p);
+  const menuEl = p.$(navbarThemeMenuSel);
+  await checkThemeMenu(0, menuEl);
+  await menuEl.$aButton('Dark theme').click();
+  await checkTheme(p, true);
+
+  // Check settings are saved.
+  await p.reload();
+  await checkTheme(p, true);
+});
+
+test('Navbar - Change to device theme (light)', async ({ page }) => {
+  const p = $(page);
+  await p.goto('/', null);
+  await clickNavbarThemeMenu(p);
+  const menuEl = p.$(navbarThemeMenuSel);
+  await checkThemeMenu(0, menuEl);
+  await menuEl.$aButton('Device theme').click();
+  await checkTheme(p, false);
+
+  // Check settings are saved.
+  await p.reload();
+  await checkTheme(p, false);
+});
+
+test('Navbar - Change to device theme (dark)', async ({ page }) => {
+  const p = $(page);
+  await p.c.emulateMedia({ colorScheme: 'dark' });
+  await p.goto('/', null);
+  await clickNavbarThemeMenu(p);
+  const menuEl = p.$(navbarThemeMenuSel);
+  await checkThemeMenu(0, menuEl);
+  await menuEl.$aButton('Device theme').click();
+  await checkTheme(p, true);
+
+  // Check settings are saved.
+  await p.reload();
+  await checkTheme(p, true);
+});
+
+test('Navbar - Device theme monitors system theme change', async ({ page }) => {
+  const p = $(page);
+  await p.goto('/', null);
+  await clickNavbarThemeMenu(p);
+  const menuEl = p.$(navbarThemeMenuSel);
+  await checkThemeMenu(0, menuEl);
+  await menuEl.$aButton('Device theme').click();
+
+  // Check settings are saved.
+  await p.reload();
+  await p.c.emulateMedia({ colorScheme: 'dark' });
+  await checkTheme(p, true);
+  await p.c.emulateMedia({ colorScheme: 'light' });
+  await checkTheme(p, false);
 });
