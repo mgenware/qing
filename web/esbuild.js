538e85537bcebb9200542325a661178e3a7e8235
Mgen <m@mgenware.com>
2022-08-03 23:05:02 +0800

    Minify HTML literals in esbuild

---
+++ web/esbuild.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 The Qing Project. All rights reserved.
+ *
+ * Use of this source code is governed by a license that can
+ * be found in the LICENSE file.
+ */
+
+import esbuild from 'esbuild';
+import { minifyHTMLLiteralsPlugin } from 'esbuild-plugin-minify-html-literals';
+
+const entryPoints = [
+  'core',
+  'm/mEntry',
+  'mx/mxEntry',
+  'post/postEntry',
+  'profile/profileEntry',
+  'auth/authEntry',
+  'home/homeStdEntry',
+  'home/homeFrmEntry',
+  'forum/forumEntry',
+  'forum/forumSettingsEntry',
+  'lang/langEntry',
+  'devPage/devPageEntry',
+].map((s) => `dist/${s}.js`);
+
+await esbuild.build({
+  entryPoints,
+  bundle: true,
+  outdir: '../userland/static/g/app',
+  define: {
+    'window.__qing_dev__': true,
+    this: 'window',
+  },
+  target: [
+    // Based on https://caniuse.com/mdn-css_selectors_part
+    'es2020',
+    'chrome73',
+    'edge79',
+    'firefox72',
+    'ios13.4',
+    'safari13.1',
+  ],
+  plugins: [minifyHTMLLiteralsPlugin()],
+});
