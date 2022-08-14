803bf490fecad0eb664d8cef6040ac53eacd2a50
Mgen <m@mgenware.com>
2022-08-14 15:09:20 +0800

    Add tests for profile page

---
+++ it/br/settings/profile.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022 The Qing Project. All rights reserved.
+ *
+ * Use of this source code is governed by a license that can
+ * be found in the LICENSE file.
+ */
+
+import { test, usr, $ } from 'br';
+import * as nbm from 'br/com/navbar/menu';
+import * as ivh from 'br/com/forms/inputViewHelper';
+
+test('Settings - Profile', async ({ page }) => {
+  const p = $(page);
+  await p.goto('/', usr.user);
+
+  await nbm.userMenuBtn(p).click();
+  await nbm.userMenuEl(p).$aButton('Settings').click();
+
+  const rootEl = p.$('edit-profile-app');
+  // Profile menu item highlighted.
+  await rootEl.$('a[class="link-active"][href="/m/settings/profile"]').e.toBeVisible();
+
+  // Profile picture.
+  await rootEl
+    .$(
+      'img[width="250"][height="250"][class="avatar-l profile-img"][src="/res/avatars/2u/250_user.png"]',
+    )
+    .e.toBeVisible();
+
+  await ivh.shouldHaveValue(rootEl.$inputView('Name'), 'USER');
+  await ivh.shouldHaveValue(rootEl.$inputView('URL'), 'USER_WEBSITE');
+  await ivh.shouldHaveValue(rootEl.$inputView('Company'), 'USER_COMPANY');
+  await ivh.shouldHaveValue(rootEl.$inputView('Location'), 'USER_LOC');
+});
--- it/br/sql/required_users.sql
+++ it/br/sql/required_users.sql
@@ -7,10 +7,10 @@
 
 INSERT INTO `user` (`id`, `email`, `name`, `icon_name`, `created_at`, `company`, `website`, `location`, `bio`, `admin`)
 VALUES
-	(101, 'admin@mgenware.com', 'ADMIN', 'admin.png', '2019-01-31 10:20:30', 'ADMIN_COMPANY', 'ADMIN_WEBSITE', 'ADMIN_LOC', NULL, 1),
-	(102, 'user@mgenware.com', 'USER', 'user.png', '2019-01-31 10:20:30', 'USER_COMPANY', 'USER_WEBSITE', 'USER_LOC', NULL, 0),
-	(103, 'admin2@mgenware.com', 'ADMIN2', 'admin2.png', '2019-01-31 10:20:30', 'ADMIN2_COMPANY', 'ADMIN2_WEBSITE', 'ADMIN2_LOC', NULL, 1),
-	(104, 'user2@mgenware.com', 'USER2', 'user2.png', '2019-01-31 10:20:30', 'USER2_COMPANY', 'USER2_WEBSITE', 'USER2_LOC', NULL, 0);
+	(101, 'admin@mgenware.com', 'ADMIN', 'admin.png', '2019-01-31 10:20:30', 'ADMIN_COMPANY', 'ADMIN_WEBSITE', 'ADMIN_LOC', 'ADMIN_BIO', 1),
+	(102, 'user@mgenware.com', 'USER', 'user.png', '2019-01-31 10:20:30', 'USER_COMPANY', 'USER_WEBSITE', 'USER_LOC', 'USER_BIO', 0),
+	(103, 'admin2@mgenware.com', 'ADMIN2', 'admin2.png', '2019-01-31 10:20:30', 'ADMIN2_COMPANY', 'ADMIN2_WEBSITE', 'ADMIN2_LOC', 'ADMIN2_BIO', 1),
+	(104, 'user2@mgenware.com', 'USER2', 'user2.png', '2019-01-31 10:20:30', 'USER2_COMPANY', 'USER2_WEBSITE', 'USER2_LOC', 'USER2_BIO', 0);
 
 INSERT INTO `user_stats` (`id`)
 VALUES (101), (102), (103), (104);
