2e2cfaa6267f03877bbeb5f7d3053ac8b50925a4
Mgen <m@mgenware.com>
2022-09-04 15:20:23 +0800

    Clean and rebuild all SOD files when arg0 is empty

--- lib/dev/sod/build.ts
+++ lib/dev/sod/build.ts
@@ -8,60 +8,69 @@
 import * as mfs from 'm-fs';
 import np from 'path';
 import yaml from 'js-yaml';
+import { globby } from 'globby';
+import { deleteAsync } from 'del';
 import * as qdu from '@qing/devutil';
 import * as cm from './common.js';
 import * as go from './goUtil.js';
 import * as ts from './tsUtil.js';
 
 const yamlExt = '.yaml';
+const sodSrcDir = './sod/objects';
 
 function print(s: string) {
   // eslint-disable-next-line no-console
   console.log(s);
 }
 
-const input = process.argv.slice(2)[0];
-if (!input) {
-  print('No input.');
-  process.exit(1);
+function cutYamlExt(s: string) {
+  return s.substring(0, s.length - yamlExt.length);
+}
+
+const arg0 = process.argv.slice(2)[0];
+
+async function build(name: string) {
+  const fullInput = qdu.sodPath(name + yamlExt);
+  const rawSource = yaml.load(await mfs.readTextFileAsync(fullInput));
+  if (typeof rawSource !== 'object' || Array.isArray(rawSource)) {
+    throw new Error(`Source YAML must be an object. Got ${JSON.stringify(rawSource)}`);
+  }
+  const srcDict = rawSource as cm.SourceDict;
+  const relDir = np.dirname(name);
+  const pkgName = np.basename(relDir) + 'Sod';
+  const webFile = np.join(qdu.webSodPath(), name) + '.ts';
+
+  // NOTE: Unlike .ts file, .go files are put in an extra folder named the same
+  // as the extracted package name.
+  const serverFile = np.join(qdu.serverSodPath(), name + '.go');
+
+  await Promise.all([
+    mfs.writeFileAsync(serverFile, go.goCode(name, pkgName, srcDict)),
+    mfs.writeFileAsync(webFile, ts.tsCode(name, srcDict)),
+  ]);
+  print('Files written:');
+  print(serverFile);
+  print(webFile);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   try {
-    const fullInput = qdu.sodPath(input + yamlExt);
-    const rawSource = yaml.load(await mfs.readTextFileAsync(fullInput));
-    if (typeof rawSource !== 'object' || Array.isArray(rawSource)) {
-      throw new Error(`Source YAML must be an object. Got ${JSON.stringify(rawSource)}`);
-    }
-
-    // eslint-disable-next-line no-inner-declarations
-    function getAttr(key: string): string | null {
-      return cm.popDictAttribute(rawSource as Record<string, string>, key);
-    }
-
-    const srcDict = rawSource as cm.SourceDict;
-    const relDir = np.dirname(input);
-    const pkgName = np.basename(relDir) + 'Sod';
-    const webFile = np.join(qdu.webSodPath(), input) + '.ts';
-
-    const goOutDir = getAttr(go.goOutDirAttr);
-    let serverFile: string;
-    if (goOutDir) {
-      serverFile = np.join(qdu.serverPath(), goOutDir, np.basename(input) + '.go');
+    if (arg0) {
+      await build(arg0);
     } else {
-      // NOTE: Unlike .ts file, .go files are put in an extra folder named the same
-      // as the extracted package name.
-      serverFile = np.join(qdu.serverSodPath(), input + '.go');
-    }
+      const longFiles = await globby(`${sodSrcDir}/**/*.yaml`);
+      /*
+        Example output:
+        'sod/objects/app/appRawSettings.yaml',
+        'sod/objects/app/rawMainPageWind.yaml',
+        'sod/objects/cmt/cmt.yaml',
+      */
+      const files = longFiles.map((f) => cutYamlExt(np.relative(sodSrcDir, f)));
 
-    await Promise.all([
-      mfs.writeFileAsync(serverFile, go.goCode(input, pkgName, srcDict)),
-      mfs.writeFileAsync(webFile, ts.tsCode(input, srcDict)),
-    ]);
-    print('Files written:');
-    print(serverFile);
-    print(webFile);
+      await deleteAsync([qdu.serverSodPath(), qdu.webSodPath()], { force: true });
+      await Promise.all(files.map((f) => build(f)));
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
