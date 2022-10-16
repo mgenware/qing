ce44bfaff8e6168402bdbd6012934853493c8e88
Mgen <m@mgenware.com>
2022-10-17 00:55:15 +0800

    Switch primary br tests to webkit

--- it/daizong.config.js
+++ it/daizong.config.js
@@ -5,7 +5,7 @@
  * be found in the LICENSE file.
  */
 
-const brCmd = 'npx playwright test --project=chromium -c dist';
+const brCmd = 'npx playwright test --project=webkit -c dist';
 
 export default {
   lint: 'eslint --max-warnings 0 --ext .ts .',
