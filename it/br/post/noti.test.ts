8ece379991af9cbb184ba00fb92c74754c1d595d
Mgen <m@mgenware.com>
2022-10-29 02:00:47 +0800

    Fix failing tests

--- it/br/post/noti.test.ts
+++ it/br/post/noti.test.ts
@@ -7,31 +7,28 @@
 
 import * as br from 'br';
 import { CmtFixtureWrapper } from 'br/cmt/common';
-import { getEmail, newUser } from 'helper/user';
+import { getEmail } from 'helper/user';
 import * as mh from 'helper/mail';
 import postCmtFixture from './postCmtFixture';
 import * as def from 'base/def';
 import * as act from '../cmt/actions';
+import * as cm from '../cmt/common';
 
 const w = new CmtFixtureWrapper('Post', postCmtFixture);
 
-w.test('Post noti', { author: 'new' }, async ({ p }) => {
-  // Create a post with a tmp user.
-  await newUser(async (u) => {
-    // Add a cmt with user1.
-    await p.reload(br.usr.user);
-    const cmtApp = await w.getCmtApp(p);
-    await act.writeCmt(p, { cmtApp, content: def.sd.content });
+w.test('Post noti', { author: 'new', viewer: br.usr.user }, async ({ p, author }) => {
+  const cmtApp = await w.getCmtApp(p);
+  await act.writeCmt(p, { cmtApp, content: def.sd.content });
+  const cmtID = await cm.getCmtIDAsync({ cmtEl: cm.getTopCmt({ cmtApp }) });
 
-    const postLink = w.getHostURL(p);
-    const email = await getEmail(u);
-    const mail = await mh.getLatest({ email });
-    br.expect(mail.title).toBe(
-      'USER has replied to your comment in "<p>title</p><script>alert(\'-39\')</script>".',
-    );
-    br.expect(mail.content.trim()).toBe(`<p>
-<span>USER has replied to your comment in &#34;&lt;p&gt;title&lt;/p&gt;&lt;script&gt;alert(&#39;-39&#39;)&lt;/script&gt;&#34;.</span>
-<a href="${postLink}">Click here to view it on Qing.</a>
+  const postLink = w.getHostURL(p);
+  const email = await getEmail(author);
+  const mail = await mh.getLatest({ email });
+  br.expect(mail.title).toBe(
+    'USER has replied to your post "<p>title</p><script>alert(\'-39\')</script>".',
+  );
+  br.expect(mail.content.trim()).toBe(`<p>
+  <span>USER has replied to your post &#34;&lt;p&gt;title&lt;/p&gt;&lt;script&gt;alert(&#39;-39&#39;)&lt;/script&gt;&#34;.</span>
+  <a href="${postLink}?cmt=${cmtID}">Click here to view it on Qing.</a>
 </p>`);
-  });
 });
