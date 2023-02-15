7b12c2ffdc3d16921995e4fdd94a0a84e45d111c
Mgen <m@mgenware.com>
2023-02-16 02:11:05 +0800

    Remove `preserveWatchOutput` from tsc command

--- web/daizong.config.js
+++ web/daizong.config.js
@@ -7,7 +7,8 @@
 
 const turboBuildCmd = 'tsc -p ./tsconfig-turbo.json';
 const utCmd = 'wtr "./dist-turbo/**/*.test.js" --node-resolve --playwright --browsers chromium';
-const tscW = 'tsc --preserveWatchOutput -w';
+const tsc = 'tsc -noEmit';
+const tscW = `${tsc} -w`;
 const devEnv = {
   NODE_ENV: 'development',
 };
