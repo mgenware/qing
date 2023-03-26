8899f7ae87b4146fa6cad13336d02e589144d196
Mgen <m@mgenware.com>
2023-03-27 00:10:32 +0800

    Make BR test account create_date sortable

--- it/sql/required_users.sql
+++ it/sql/required_users.sql
@@ -7,9 +7,9 @@
 
 INSERT INTO `user` (`id`, `email`, `name`, `icon_name`, `created_at`, `company`, `website`, `location`, `bio`, `admin`, `reg_lang`)
 VALUES
-	(101, 'admin@mgenware.com', 'ADMIN', 'admin.png', '2019-01-31 10:20:30', 'ADMIN_COMPANY', 'ADMIN_WEBSITE', 'ADMIN_LOC', '&lt;ADMIN_BIO&gt;', 1, 'en'),
-	(102, 'user@mgenware.com', 'USER', 'user.png', '2019-01-31 10:20:30', 'USER_COMPANY', 'USER_WEBSITE', 'USER_LOC', '&lt;USER_BIO&gt;', 0, 'en'),
-	(103, 'admin2@mgenware.com', 'ADMIN2', 'admin2.png', '2019-01-31 10:20:30', 'ADMIN2_COMPANY', 'ADMIN2_WEBSITE', 'ADMIN2_LOC', '&lt;ADMIN2_BIO&gt;', 1, 'en'),
+	(101, 'admin@mgenware.com', 'ADMIN', 'admin.png', '2019-01-28 10:20:30', 'ADMIN_COMPANY', 'ADMIN_WEBSITE', 'ADMIN_LOC', '&lt;ADMIN_BIO&gt;', 1, 'en'),
+	(102, 'user@mgenware.com', 'USER', 'user.png', '2019-01-29 10:20:30', 'USER_COMPANY', 'USER_WEBSITE', 'USER_LOC', '&lt;USER_BIO&gt;', 0, 'en'),
+	(103, 'admin2@mgenware.com', 'ADMIN2', 'admin2.png', '2019-01-30 10:20:30', 'ADMIN2_COMPANY', 'ADMIN2_WEBSITE', 'ADMIN2_LOC', '&lt;ADMIN2_BIO&gt;', 1, 'en'),
 	(104, 'user2@mgenware.com', 'USER2', 'user2.png', '2019-01-31 10:20:30', 'USER2_COMPANY', 'USER2_WEBSITE', 'USER2_LOC', '&lt;USER2_BIO&gt;', 0, 'en');
 
 INSERT INTO `user_stats` (`id`)
