4480f890fc020a3cb9a6011c7f05667cf67aa876
Mgen <m@mgenware.com>
2023-08-13 23:48:51 +0800

    Test everyone and personal modes

--- it/br/home/home.test.ts
+++ it/br/home/home.test.ts
@@ -14,109 +14,121 @@ import { appDef } from '@qing/def';
 const homeItemSel = '.fi-item';
 const page2URL = '/?page=2';
 
-test('Home page - Community - One page', async ({ page }) => {
-  const p = $(page);
-  const prefix = '_br_home_com_one_page_';
+enum HomePageMode {
+  personal,
+  everyone,
+}
 
-  try {
-    const posts = await batchNewPosts({
-      user: usr.user,
-      count: 2,
-      title: 'TITLE',
-      content: 'CONTENT',
-      summary: 'SUMMARY',
-      prefix,
-      startingDate: new Date('2006-02-01'),
-    });
+function testHomePage(mode: HomePageMode) {
+  const modeText = HomePageMode[mode];
+
+  test(`Home page - ${modeText} - One page`, async ({ page }) => {
+    const p = $(page);
+    const prefix = `_br_home_${modeText}_1_page_`;
+
+    try {
+      const posts = await batchNewPosts({
+        user: usr.user,
+        count: 2,
+        title: 'TITLE',
+        content: 'CONTENT',
+        summary: 'SUMMARY',
+        prefix,
+        startingDate: new Date('2006-02-01'),
+      });
+
+      await p.goto('/', null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
+      await pb.shouldNotExist(p.body);
+
+      const items = p.$$(homeItemSel);
+      await items.shouldHaveCount(2);
+
+      await cm.checkHomeItem(items.item(0), {
+        user: usr.user,
+        title: `${prefix}TITLE_2`,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        link: posts[0]!.link,
+      });
+      await cm.checkHomeItem(items.item(1), {
+        user: usr.user,
+        title: `${prefix}TITLE_1`,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        link: posts[1]!.link,
+      });
+    } finally {
+      await deletePostsByPrefix(prefix);
+    }
+  });
+
+  test(`Home page - ${modeText} - 2 pages`, async ({ page }) => {
+    const p = $(page);
+    const prefix = `_br_home_${modeText}_2_pages_`;
+
+    try {
+      const posts = await batchNewPosts({
+        user: usr.user,
+        count: 3,
+        title: 'TITLE',
+        content: 'CONTENT',
+        summary: 'SUMMARY',
+        prefix,
+        startingDate: new Date('2006-02-01'),
+      });
+
+      await p.goto('/', null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
+
+      const items = p.$$(homeItemSel);
+      await items.shouldHaveCount(2);
+
+      await cm.checkHomeItem(items.item(0), {
+        user: usr.user,
+        title: `${prefix}TITLE_3`,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        link: posts[0]!.link,
+      });
+      await cm.checkHomeItem(items.item(1), {
+        user: usr.user,
+        title: `${prefix}TITLE_2`,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        link: posts[1]!.link,
+      });
+
+      // Check page bar.
+      await pb.check(p.body, { rightLink: page2URL });
+      // Don't click the next button, it doesn't has `brHomePrefixParam` in it.
+      // Instead, compose a new URL and use goto to navigate to it.
+      await p.goto(page2URL, null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
+
+      await cm.checkHomeItem(items.item(0), {
+        user: usr.user,
+        title: `${prefix}TITLE_1`,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        link: posts[2]!.link,
+      });
+
+      // Check page bar.
+      await pb.check(p.body, { leftLink: '/' });
+    } finally {
+      await deletePostsByPrefix(prefix);
+    }
+  });
+
+  test(`Home page - ${modeText} - No content`, async ({ page }) => {
+    const p = $(page);
+    const prefix = '__no_content__';
 
     await p.goto('/', null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
+
+    // No page bar.
     await pb.shouldNotExist(p.body);
 
+    // No feed items.
     const items = p.$$(homeItemSel);
-    await items.shouldHaveCount(2);
+    await items.shouldHaveCount(0);
 
-    await cm.checkHomeItem(items.item(0), {
-      user: usr.user,
-      title: `${prefix}TITLE_2`,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      link: posts[0]!.link,
-    });
-    await cm.checkHomeItem(items.item(1), {
-      user: usr.user,
-      title: `${prefix}TITLE_1`,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      link: posts[1]!.link,
-    });
-  } finally {
-    await deletePostsByPrefix(prefix);
-  }
-});
+    await p.$('no-content-view').shouldExist();
+  });
+}
 
-test('Home page - Community - 2 pages', async ({ page }) => {
-  const p = $(page);
-  const prefix = '_br_home_com_2_pages_';
-
-  try {
-    const posts = await batchNewPosts({
-      user: usr.user,
-      count: 3,
-      title: 'TITLE',
-      content: 'CONTENT',
-      summary: 'SUMMARY',
-      prefix,
-      startingDate: new Date('2006-02-01'),
-    });
-
-    await p.goto('/', null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
-
-    const items = p.$$(homeItemSel);
-    await items.shouldHaveCount(2);
-
-    await cm.checkHomeItem(items.item(0), {
-      user: usr.user,
-      title: `${prefix}TITLE_3`,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      link: posts[0]!.link,
-    });
-    await cm.checkHomeItem(items.item(1), {
-      user: usr.user,
-      title: `${prefix}TITLE_2`,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      link: posts[1]!.link,
-    });
-
-    // Check page bar.
-    await pb.check(p.body, { rightLink: page2URL });
-    // Don't click the next button, it doesn't has `brHomePrefixParam` in it.
-    // Instead, compose a new URL and use goto to navigate to it.
-    await p.goto(page2URL, null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
-
-    await cm.checkHomeItem(items.item(0), {
-      user: usr.user,
-      title: `${prefix}TITLE_1`,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      link: posts[2]!.link,
-    });
-
-    // Check page bar.
-    await pb.check(p.body, { leftLink: '/' });
-  } finally {
-    await deletePostsByPrefix(prefix);
-  }
-});
-
-test('Home page - Community - No content', async ({ page }) => {
-  const p = $(page);
-  const prefix = '__no_content__';
-
-  await p.goto('/', null, { params: { [appDef.brHomePrefixParam]: `${prefix}%` } });
-
-  // No page bar.
-  await pb.shouldNotExist(p.body);
-
-  // No feed items.
-  const items = p.$$(homeItemSel);
-  await items.shouldHaveCount(0);
-
-  await p.$('no-content-view').shouldExist();
-});
+testHomePage(HomePageMode.everyone);
+testHomePage(HomePageMode.personal);
